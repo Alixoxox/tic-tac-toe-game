3322cb95347bf043d247c6b16d7cf71d434a0448
Alixoxox <sufyanwajid05@gmail.com>
2024-04-28 17:42:08 +0500

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,14 +1,5 @@
 import random
 
-board = ['-','-','-',
-         '-','-','-',
-         '-','-','-'
-        ]
-player= "X"
-ai_marker='O'
-winner = None
-
-## BOARD LOGIC
 def print_board(board):
     print(board[0]+' |', board[1]+' |', board[2])
     print('---------')
@@ -16,98 +7,120 @@ def print_board(board):
     print('---------')
     print(board[6]+' |', board[7]+' |', board[8])
 
-## PLAYER
 def player_move(board):
-    global player
-    global winner
-    p = int(input("Enter the location where you want to place (from 1-9): "))
-    if 9 >= p >= 1:
-        if board[p-1] == '-':
-            board[p-1] = player
+    p = input("Enter the location where you want to place (from 1-9): ")
+    if p.isdigit():
+        p = int(p)
+        if 9 >= p >= 1:
+            if board[p-1] == '-':
+                board[p-1] = 'X'
+                return True
+            else:
+                print("-Cell already occupied. Try again.")
+                return False
         else:
-            print("Cell already occupied. Try again.")
-            return player_move(board)
-    elif p==0:
-        print("Try again. You entered an invalid position.")
-        return player_move(board)
+            print("-Try again. You entered an invalid position.")
+            return False
     else:
-        print("Try again. You entered an invalid position.")
-        return player_move(board)
-#AI
+        print("-Try again. You entered an invalid position.")
+        return False
+
 def ai_move(board):
-    global winner
-    position = random.randint(0, 8)
-    if board[position] == '-':
-        board[position] = ai_marker
-    else:
-        ai_move(board)
+    # Check for winning moves
+    for i in range(9):
+        if board[i] == '-':
+            board[i] = 'O'
+            if check_win(board):
+                return
+            board[i] = '-'
 
-def alternation(board):
-    global player
-    global winner
-    if player=='X' in board:
-        if check_win is True:
-            exit
-        else:
-            return ai_marker 
-    elif player=='X' not in board:
-        return player(board)
+    # Check for blocking moves
+    for i in range(9):
+        if board[i] == '-':
+            board[i] = 'X'
+            if check_win(board):
+                board[i] = 'O'
+                return
+            board[i] = '-'
 
+    # Choose a random move if no winning or blocking moves
+    empty_cells = [i for i in range(9) if board[i] == '-']
+    if empty_cells:
+        move = random.choice(empty_cells)
+        board[move] = 'O'
+
+
+def check_win(board):
+    if winlogic_row(board) or winlogic_column(board) or winlogic_diag(board):
+        return True
+    return False
 
 def winlogic_row(board):
-    ## for row
-    global winner
     if board[0] == board[1] == board[2] and board[0] != '-':
-        winner = board[0]
         return True
     elif board[3] == board[4] == board[5] and board[4] != '-':
-        winner = board[3]
         return True
     elif board[6] == board[7] == board[8] and board[8] != '-':
-        winner = board[6]
         return True
     return False
 
 def winlogic_column(board):
-    ## for column
-    global winner
     if board[0] == board[3] == board[6] and board[0] != '-':
-        winner = board[0]
         return True
-    elif board[2] == board[5] == board[8] and board[2] != '-':
-        winner = board[2]
+    elif board[1] == board[4] == board[7] and board[4] != '-':
+        return True
+    elif board[2] == board[5] == board[8] and board[8] != '-':
         return True
     return False
 
 def winlogic_diag(board):
-    ## for diagonals
-    global winner
     if board[0] == board[4] == board[8] and board[0] != '-':
-        winner = board[0]
         return True
     elif board[2] == board[4] == board[6] and board[2] != '-':
-        winner = board[2]
         return True
     return False
 
-def check_win(board):
-    global winner
-    if winlogic_row(board) or winlogic_column(board) or winlogic_diag(board):
-        print(f"{winner} has won")
-        exit()
+def check_draw(board):
+    for cell in board:
+        if cell == '-':
+            return False
+    return True
+
+board = ['-','-','-',
+         '-','-','-',
+         '-','-','-'
+        ]
+
+player= "X"
+ai_marker='O'
+winner = None
 
 while True:
     print_board(board)
-    player_move(board)
-    if check_win(board):
-        print_board(board)
-        print(f"{winner} has won!")
-        break
-
-    print_board(board)
-    print("----------------xxxxx--------------------")
-    ai_move(board)
-    if check_win(board):
-        print_board(board)
-        print(f"{winner} has won!")
-        break
+    print("."*30)
+    if player == "X":
+        if player_move(board):
+            if check_win(board):
+                print_board(board)
+                print("."*30)
+                print("--Player X has won!")
+                break
+            if check_draw(board):
+                print_board(board)
+                print("."*30)
+                print("--It's a draw!")
+                break
+            player = "O"
+    else:
+        ai_move(board)
+        if check_win(board):
+            print_board(board)
+            print("."*30)
+            print("--Player O has won!")
+            break
+        if check_draw(board):
+            print_board(board)
+            print("."*30)
+            print("--It's a draw!")
+            break
+        player = "X"
